9b29ded25c3fea9adf25b04d5f863c282f668c97
Rodolfo Nogueira <rodolfojnn@hotmail.com>
2026-05-11 11:07:02 -0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -39,6 +39,9 @@ module.exports = defineConfig([
       angular.configs.templateRecommended,
       angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      '@angular-eslint/template/click-events-have-key-events': 'off',
+      '@angular-eslint/template/interactive-supports-focus': 'off',
+    },
   }
 ]);
