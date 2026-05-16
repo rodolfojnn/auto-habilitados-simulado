846a9b64bd9d13227ac639c2acf895c3ae486bcc
Rodolfo Nogueira <rodolfojnn@hotmail.com>
2026-05-16 15:36:06 -0300

    icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'br.com.simulado.cnh.brasil',
-  appName: 'simulado-cnh-do-brasil',
+  appName: 'Simulado CNH do Brasil',
   webDir: 'dist/app/browser',
   // server: {
   //   url: 'http://192.168.1.64:4255',
